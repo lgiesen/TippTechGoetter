67a5312325f3a3e4b5ad51305704cf2345972b11
Leo Giesen <leo.richard.giesen@gmail.com>
2020-01-21 22:21:18 +0100

    :rocket: scraping from another website for a more precice prediction

--- webscrapePrediction.py
+++ webscrapePrediction.py
@@ -3,22 +3,77 @@ import requests
 from bs4 import BeautifulSoup
 # import csv writer to be able to write our data to a CSV-file
 from csv import writer
+# ------------------------------------------- forebet -------------------------------------------
 
 # get the website from the website
-website = requests.get('https://www.forebet.com/en/football-tips-and-predictions-for-germany/Bundesliga')
+forebet = requests.get('https://www.forebet.com/en/football-tips-and-predictions-for-germany/Bundesliga')
 # initialize BeautifulSoup with the website. Also let bs know that it needs to parse html
-soup = BeautifulSoup(website.text, 'html.parser')
-
+fb = BeautifulSoup(forebet.text, 'html.parser')
+# -------------------- Teams in match order --------------------
 # create list with all the teams
 matchDay = []
 # first filter to the table with the class "schema tblen", then find the specific tr with class "tr_0"
 # and find the name of the team in the span element where the attribute "itemprop" is set to "name"
-teamsAsList = soup.find('table', {'class': "schema tblen"}) \
+teamsAsList = fb.find('table', {'class': "schema tblen"}) \
     .find('tr', {"class": "tr_0"}) \
     .find_all('span', attrs={"itemprop": "name"})
 for team in teamsAsList:
     matchDay.append(team.get_text())
+# -------------------- Prediction forebet --------------------
+# P = probability for home/draw/away-result
+forebetPrediction = fb.find('table', {'class': "schema tblen"}) \
+    .find('tr', {"class": "tr_0"}) \
+    .find_all('td', {"class": ""})
+forebetPrediction = forebetPrediction[:27]
+result = ""  # specific game outcome
+allResults = []  # array with all outcomes
+counter = 1  # counter for making sure to only use the desired probabilities and not other info
+# convert all html tags (<td><b>57</b></td>) to int (57)
+for probability in forebetPrediction:
+    index = forebetPrediction.index(probability)
+    forebetPrediction[index] = int(forebetPrediction[index].get_text())
 
+# ------------------------------------------- kickform -------------------------------------------
+# -------------------- Prediction kickform --------------------
+
+kickform = requests.get('https://www.kickform.de/')
+# initialize BeautifulSoup with the website. Also let bs know that it needs to parse html
+kf = BeautifulSoup(kickform.text, 'html.parser')
+
+# Predictions forebet
+# P = probability for home/draw/away-result
+kickformPrediction = kf.find_all('div', {"class": "prognose-balken"})
+for probability in kickformPrediction:
+    index = kickformPrediction.index(probability)
+    kickformPrediction[index] = int(kickformPrediction[index].get_text().replace("%", ""))
+# too many predictions -> reduce/slice down to the ones we need for this matchday
+kickformPrediction = kickformPrediction[:27]
+
+# get the mean of both lists
+probabilities = [(g + h) / 2 for g, h in zip(forebetPrediction, kickformPrediction)]
+
+# check what is the maximum probability
+for probability in probabilities:
+    # find the maximum probability and set the winner accordingly
+    if ((probabilities[0] >= probabilities[1]) &
+            (probabilities[0] >= probabilities[2])):
+        result = "Home"
+    elif ((probabilities[1] >= probabilities[0]) &
+          (probabilities[1] >= probabilities[2])):
+        result = "Draw"
+    else:
+        result = "Away"
+    # take away the first three possibilities because we already evaluated them for this match
+    probabilities = probabilities[3:]
+    # add the result to allResults
+    allResults.append(result)
+
+    # print(str(counter) + " " + result) # Print all results
+    counter += 1
+    if counter >= 10:
+        break
+
+# -------------------- write to CSV --------------------
 # open or create this csv file and w = write to it
 with open('predictions.csv', 'w') as csv_file:
     # use the imported writer to write posts.csv as csv_file
@@ -26,33 +81,6 @@ with open('predictions.csv', 'w') as csv_file:
     headers = ['HomeTeam', 'AwayTeam', 'Result']
     # write the headers
     csv_writer.writerow(headers)
-    # write all the variables in each row
-
-    # Predictions
-    # P = probability for home/draw/away-result
-    probabilities = soup.find('table', {'class': "schema tblen"}) \
-        .find('tr', {"class": "tr_0"}) \
-        .find_all('td', {"class": ""})
-    result = ""  # specific game outcome
-    allResults = []  # array with all outcomes
-    counter = 1  # counter for making sure to only use the desired probabilities and not other info
-    for probability in probabilities:
-        # find the maximum probability and set the winner accordingly
-        if ((probabilities[0].get_text() >= probabilities[1].get_text()) &
-                (probabilities[0].get_text() >= probabilities[2].get_text())):
-            result = "Home"
-        elif ((probabilities[1].get_text() >= probabilities[0].get_text()) &
-              (probabilities[1].get_text() >= probabilities[2].get_text())):
-            result = "Draw"
-        else:
-            result = "Away"
-        probabilities = probabilities[3:]
-        allResults.append(result)
-
-        # print(str(counter) + " " + result) # Print all results
-        counter += 1
-        if counter >= 10:
-            break
 
     # Teams
     homeTeam = ""
@@ -78,3 +106,16 @@ with open('predictions.csv', 'w') as csv_file:
         # match day by saying we want to stop after we have got all 18 teams, who play on that match day
         if teamIndex >= 17:
             break
+
+# soup.body.title
+# or find('h3') [prints first elem] or findAll() [findAll('div)[indexORe.g.'href']]
+# soup.find(id='idName') or class_='className'
+# soup.select('idName')  --- returns a list so just add a [0] if needed
+# soup.find(attrs={"attributE": "valuE"})
+# print without html-tags:
+# soup.find(id='idName').get_text()
+
+# navigate
+# !! soup.body.contents[1].contents[2].find_next_sibling().find_previous_sibling().find_next_sibling('p').find_parent()
+
+# !! get rid of linebreaks ('\n') by using .replace('\n', '')
